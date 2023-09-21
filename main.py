1501c6c3a98181a363a7eacd3fd396670080c1f2
abilimckl1 <mackhemchuah@gmail.com>
2023-09-21 16:57:20 +0800

    revert code

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import requests
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = 'gevent'
+async_mode = None
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode=async_mode)
-thread = 4
+thread = None
 thread_lock = Lock()
 
 urlBTCByBit = 'https://api-testnet.bybit.com/v2/public/tickers?symbol=BTCUSDT'
